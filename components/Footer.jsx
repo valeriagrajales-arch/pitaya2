48e8896e865156b4ac43b5f441297a310488e1e2
valeriagrajales-arch <valeria.grajales@soyrosarista.edu.co>
2025-11-07 07:23:23 -0500

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2024 Pitaya. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
+
